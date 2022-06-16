2a0025b75c1993bd83770c88ea2ce82ff7792bf5
ahsan091 <106489956+ahsan091@users.noreply.github.com>
2022-06-17 01:36:05 +0500

    initial commit

---
+++ lab 1.cpp
@@ -0,0 +1,29 @@
+//Program to design a house
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "\n\n\t\t\t  \_______________________________" <<endl;
+	cout << "\t\t         \/\\ \t\t\t         \\" <<endl;
+	cout << "\t\t        \/  \\ \t\t\t          \\" <<endl;
+	cout << "\t\t       \/    \\ \t\t\t           \\" <<endl;
+	cout << "\t\t      \/      \\ \t\t\t            \\" <<endl;
+	cout << "\t\t     \/        \\ \t \_________\           \\" <<endl;
+	cout << "\t\t    \/          \\ \t \\  \  \\   \\           \\" <<endl;
+	cout << "\t\t   \/            \\ \t  \\\=== \\===\\           \\" <<endl;
+    cout << "\t\t  \/              \\ \t   \\____\\___\\\t        \\" <<endl;
+	cout << "\t\t \/\:______________:\\ \t\t\t         \\" <<endl;
+	cout << "\t\t\/ \:              \:\ \_______________________________\\" <<endl;
+	cout << "\t\t  \:\t\t \:\t\t\t\         \:" <<endl;
+	cout << "\t\t  \:\t\t \:\t\t\t\t \:" <<endl;
+	cout << "\t\t  \:\t\t \:\t\t\t\t \:" <<endl;
+	cout << "\t\t  \:\t\t \:\t        ___\t\t \:" <<endl;
+	cout << "\t\t  \:    \'-----\'\t \:\t       :_:_:\t\t \:" <<endl;
+	cout << "\t\t  \:    \'     '\   \:\t       :_:_:\t\t \:" <<endl;
+	cout << "\t\t  \:    \'    *'\   \:\t\t\t\t \:" <<endl;
+	cout << "\t\t  \:    \'     '\   \:\t\t\t\t \:" <<endl;
+	cout << "\t\t  \:____\'_____'\___\:_______________________________\:" <<endl;
+	 
+	return 0;
+}
